c3c699c185526875563cc99dbeb19fa34445ea72
bobista0 <borys.rybak@gmail.com>
2015-05-14 14:28:55 +0200

    Init PivotPetSound project

---
+++ PivotPetSounds/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PivotPetSounds.Resources;
+
+namespace PivotPetSounds
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ PivotPetSounds/ViewModels/SoundModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PivotPetSounds.ViewModels
+{
+    public class SoundModel
+    {
+        public SoundGroup Animals { get; set; }
+        public SoundGroup Cartoons { get; set; }
+        public SoundGroup CustomSounds { get; set; }
+
+        public bool IsDataLoaded { get; set; }
+
+        public void LoadData()
+        {
+            IsDataLoaded = true;
+        }
+    }
+}
